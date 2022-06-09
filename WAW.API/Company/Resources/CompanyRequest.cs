181547e0dfc6a65db9cbd0bdcba15a38224c7454
Angel Meneses <angelmenesest123@gmail.com>
2022-06-09 16:16:51 -0500

    feat(companies): created DTOs (resource and request) for Company

---
+++ WAW.API/Company/Resources/CompanyRequest.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WAW.API.Company.Resources;
+
+public class CompanyRequest {
+  [Required]
+  public string? Name { get; set; }
+
+  public string? Address { get; set; }
+
+  [Required]
+  public string? Email {get; set;}
+}
