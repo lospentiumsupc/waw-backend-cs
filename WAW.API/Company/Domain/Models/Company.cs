53651a3731b0b12fa5e71f1568ee6fbb98666ba9
Angel Meneses <angelmenesest123@gmail.com>
2022-06-09 16:19:09 -0500

    refactor: added nullable value types to properties

--- WAW.API/Company/Domain/Models/Company.cs
+++ WAW.API/Company/Domain/Models/Company.cs
@@ -2,7 +2,7 @@ namespace WAW.API.Company.Domain.Models;
 
 public class Company {
   public int Id { get; set; }
-  public string Name { get; set; }
-  public string Address { get; set; }
-  public string Email { get; set; }
+  public string? Name { get; set; }
+  public string? Address { get; set; }
+  public string? Email { get; set; }
 }
