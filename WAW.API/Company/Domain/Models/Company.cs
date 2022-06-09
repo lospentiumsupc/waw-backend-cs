ce8b534bd4880d7f0539954bd7972a9602cfd280
Angel Meneses <angelmenesest123@gmail.com>
2022-06-09 14:26:39 -0500

    feat(companies): implemented base Company model

---
+++ WAW.API/Company/Domain/Models/Company.cs
@@ -0,0 +1,8 @@
+namespace WAW.API.Company.Domain.Models;
+
+public class Company {
+  public int Id { get; set; }
+  public string Name { get; set; }
+  public string Address { get; set; }
+  public string Email { get; set; }
+}
