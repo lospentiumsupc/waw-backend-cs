6db3dc387cb3d319e079caef76abc2682b234e25
AdrianAlzamoraG <u202015385@upc.edu.pe>
2022-06-10 16:54:48 -0500

    feat(jobs): implemented domain class

---
+++ WAW.API/Job/Domain/Models/Offer.cs
@@ -0,0 +1,12 @@
+using WAW.API.Shared.Domain.Model;
+
+namespace WAW.API.Job.Domain.Models;
+
+public class Offer : BaseModel {
+
+    public string Title { get; set; }
+    public string Image { get; set; }
+    public string? Description { get; set; }
+    public string SalaryRange { get; set; }
+    public bool Status { get; set; }
+}
