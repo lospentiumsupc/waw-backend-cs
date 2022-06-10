bd6dd2c6428b049defdd5c66886e63592597f779
AdrianAlzamoraG <u202015385@upc.edu.pe>
2022-06-10 14:09:30 -0500

    feat(migration): migration was implemented to create the table offers

--- WAW.API/Migrations/20220610184650_CreateJobs.cs
+++ WAW.API/Migrations/20220610184650_CreateJobs.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -6,14 +7,27 @@ namespace WAW.API.Migrations
 {
     public partial class CreateJobs : Migration
     {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
+        protected override void Up(MigrationBuilder migrationBuilder) {
+          migrationBuilder.CreateTable(
+              name: "jobs",
+              columns: table => new {
+                id = table.Column<long>(type: "bigint", nullable: false)
+                  .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                title = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: false),
+                image = table.Column<string>(type: "varchar(500)", maxLength: 500, nullable: false),
+                description = table.Column<string>(type: "longtext", nullable: true).Annotation("MySql:CharSet", "utf8mb4"),
+                salaryRange = table.Column<string>(type: "varchar(400)",maxLength: 400, nullable: false),
+                status = table.Column<bool>(type: "boolean", nullable: false),
+
+              },
+              constraints: table => { table.PrimaryKey("p_k_jobs", x => x.id); }
+            )
+            .Annotation("MySql:Charset", "utf8mb4");
 
         }
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-
+        protected override void Down(MigrationBuilder migrationBuilder) {
+          migrationBuilder.DropTable(name: "jobs");
         }
     }
 }
