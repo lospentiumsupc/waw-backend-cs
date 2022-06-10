0e235334c04f07204e3e670fada93d3f496b0ad2
AdrianAlzamoraG <u202015385@upc.edu.pe>
2022-06-10 13:49:59 -0500

    feat(migration): migration was created to create the table offers

--- WAW.API/Migrations/20220610184650_CreateJobs.Designer.cs
+++ WAW.API/Migrations/20220610184650_CreateJobs.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WAW.API.Weather.Persistence.Contexts;
 
@@ -10,9 +11,10 @@ using WAW.API.Weather.Persistence.Contexts;
 namespace WAW.API.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220610184650_CreateJobs")]
+    partial class CreateJobs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
