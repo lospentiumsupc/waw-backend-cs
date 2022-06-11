46516b50777131cf874a9fa227a53904576aede7
Angel Meneses <angelmenesest123@gmail.com>
2022-06-10 20:30:13 -0500

    docs(companies): added swagger annotations in companies controller

--- WAW.API/Company/Controllers/CompanyController.cs
+++ WAW.API/Company/Controllers/CompanyController.cs
@@ -1,6 +1,7 @@
 using System.Net.Mime;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 using WAW.API.Company.Domain.Services;
 using WAW.API.Company.Resources;
 using WAW.API.Shared.Extensions;
@@ -12,6 +13,7 @@ using Domain.Models;
 [ApiController]
 [Route("[controller]")]
 [Produces(MediaTypeNames.Application.Json)]
+[SwaggerTag("Create, read, update and delete Companies")]
 public class CompanyController : ControllerBase {
   private readonly ICompanyService service;
   private readonly IMapper mapper;
@@ -22,12 +24,18 @@ public class CompanyController : ControllerBase {
   }
 
   [HttpGet]
+  [ProducesResponseType(typeof(IEnumerable<CompanyResource>), 200)]
   public async Task<IEnumerable<CompanyResource>> GetAll() {
     var companies = await service.ListAll();
     return mapper.Map<IEnumerable<Company>, IEnumerable<CompanyResource>>(companies);
   }
 
   [HttpPost]
+  [ProducesResponseType(typeof(CompanyResource), 201)]
+  [ProducesResponseType(typeof(List<string>), 400)]
+  [ProducesResponseType(500)]
+  [SwaggerResponse(201, "The company was created successfully", typeof(CompanyResource))]
+  [SwaggerResponse(400, "The company data is invalid")]
   public async Task<IActionResult> Post([FromBody] CompanyRequest companyRequest) {
     if (!ModelState.IsValid) return BadRequest(ModelState.GetErrorMessages());
 
@@ -37,6 +45,11 @@ public class CompanyController : ControllerBase {
   }
 
   [HttpPut("{id:int}")]
+  [ProducesResponseType(typeof(CompanyResource), 200)]
+  [ProducesResponseType(typeof(List<string>), 400)]
+  [ProducesResponseType(500)]
+  [SwaggerResponse(200, "The company was updated successfully", typeof(CompanyResource))]
+  [SwaggerResponse(400, "The company data is invalid")]
   public async Task<IActionResult> Put(int id, [FromBody] CompanyRequest companyRequest) {
     if (!ModelState.IsValid) return BadRequest(ModelState.GetErrorMessages());
 
@@ -46,6 +59,11 @@ public class CompanyController : ControllerBase {
   }
 
   [HttpDelete("{id:int}")]
+  [ProducesResponseType(typeof(CompanyResource), 200)]
+  [ProducesResponseType(typeof(List<string>), 400)]
+  [ProducesResponseType(500)]
+  [SwaggerResponse(200, "The company was deleted successfully", typeof(CompanyResource))]
+  [SwaggerResponse(400, "The selected company to delete does not exist")]
   public async Task<IActionResult> DeleteAsync(int id) {
     var result = await service.Delete(id);
     return result.ToResponse<CompanyResource>(this, mapper);
