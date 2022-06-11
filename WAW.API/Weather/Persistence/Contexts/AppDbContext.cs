c083ca247dcc4199da1dd8961ab8cd5824b6dae0
AdrianAlzamoraG <u202015385@upc.edu.pe>
2022-06-11 00:40:26 -0500

    feat(offer): offers context was implemented for persistence handling

--- WAW.API/Weather/Persistence/Contexts/AppDbContext.cs
+++ WAW.API/Weather/Persistence/Contexts/AppDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using WAW.API.Job.Domain.Models;
 using WAW.API.Shared.Extensions;
 using WAW.API.Weather.Domain.Models;
 
@@ -6,10 +7,15 @@ namespace WAW.API.Weather.Persistence.Contexts;
 
 public class AppDbContext : DbContext {
   private DbSet<Forecast>? forecasts;
-
+  private DbSet<Offer>? offers;
   public DbSet<Forecast> Forecasts {
     get => GetContext(forecasts);
     set => forecasts = value;
+  }
+
+  public DbSet<Offer> Offers {
+    get => GetContext(offers);
+    set => offers = value;
   }
 
   public AppDbContext(DbContextOptions options) : base(options) {}
@@ -23,6 +29,17 @@ public class AppDbContext : DbContext {
     forecastEntity.Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
     forecastEntity.Property(p => p.Date).IsRequired();
     forecastEntity.Property(p => p.TemperatureC).IsRequired();
+
+
+    var offerEntity = builder.Entity<Offer>();
+    offerEntity.ToTable("Offer");
+    offerEntity.HasKey(p => p.Id);
+    offerEntity.Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+    offerEntity.Property(p => p.Title).IsRequired().HasMaxLength(200);
+    offerEntity.Property(p => p.Image).IsRequired().HasMaxLength(500);
+    offerEntity.Property(p => p.Description).IsRequired();
+    offerEntity.Property(p => p.Status).IsRequired();
+
 
     builder.UseSnakeCase();
   }
