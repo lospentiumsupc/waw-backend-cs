b29fa0b3845b3c4c7a1838efda09351c0535553b
Angel Meneses <angelmenesest123@gmail.com>
2022-06-11 05:43:55 -0500

    docs(users): updated nullable attributes

--- WAW.API/Auth/Resources/UserResource.cs
+++ WAW.API/Auth/Resources/UserResource.cs
@@ -6,16 +6,16 @@ public class UserResource {
   [SwaggerSchema("User identifier", ReadOnly = true)]
   public long Id { get; set; }
 
-  [SwaggerSchema("User fullname")]
+  [SwaggerSchema("User fullname", Nullable = false)]
   public string FullName { get; set; } = string.Empty;
 
-  [SwaggerSchema("User preferred name to use in the app")]
+  [SwaggerSchema("User preferred name to use in the app", Nullable = false)]
   public string PreferredName { get; set; } = string.Empty;
 
-  [SwaggerSchema("User email")]
+  [SwaggerSchema("User email", Nullable = false)]
   public string Email { get; set; } = string.Empty;
 
-  [SwaggerSchema("User birthdate")]
+  [SwaggerSchema("User birthdate", Nullable = false)]
   public DateOnly Birthdate { get; set; }
 
   [SwaggerSchema("User location")]
