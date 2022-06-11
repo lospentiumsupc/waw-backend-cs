a259e015f1f57a19b4cdf36815f54beb5d51e9e1
Angel Meneses <angelmenesest123@gmail.com>
2022-06-11 04:19:59 -0500

    refactor: initialized not nullable properties with empty string

--- WAW.API/Job/Domain/Models/Offer.cs
+++ WAW.API/Job/Domain/Models/Offer.cs
@@ -3,10 +3,9 @@ using WAW.API.Shared.Domain.Model;
 namespace WAW.API.Job.Domain.Models;
 
 public class Offer : BaseModel {
-
-    public string Title { get; set; }
-    public string? Image { get; set; }
-    public string? Description { get; set; }
-    public string SalaryRange { get; set; }
-    public bool Status { get; set; }
+  public string Title { get; set; } = string.Empty;
+  public string? Image { get; set; }
+  public string? Description { get; set; }
+  public string SalaryRange { get; set; } = string.Empty;
+  public bool Status { get; set; }
 }
