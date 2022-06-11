19605036cf4f062f3c78ed9076a20ecaf959fb09
Kolya Castañeda <gakol8@hotmail.com>
2022-06-10 20:51:39 -0500

    feat(users): added users to dbcontext

--- WAW.API/Weather/Persistence/Contexts/AppDbContext.cs
+++ WAW.API/Weather/Persistence/Contexts/AppDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using WAW.API.Auth.Domain.Models;
 using WAW.API.Shared.Extensions;
 using WAW.API.Weather.Domain.Models;
 
@@ -6,12 +7,16 @@ namespace WAW.API.Weather.Persistence.Contexts;
 
 public class AppDbContext : DbContext {
   private DbSet<Forecast>? forecasts;
-
+  private DbSet<User>? users;
   public DbSet<Forecast> Forecasts {
     get => GetContext(forecasts);
     set => forecasts = value;
   }
 
+  public DbSet<User> Users {
+    get => GetContext(users);
+    set => users = value;
+  }
   public AppDbContext(DbContextOptions options) : base(options) {}
 
   protected override void OnModelCreating(ModelBuilder builder) {
@@ -23,6 +28,15 @@ public class AppDbContext : DbContext {
     forecastEntity.Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
     forecastEntity.Property(p => p.Date).IsRequired();
     forecastEntity.Property(p => p.TemperatureC).IsRequired();
+
+    var userEntity = builder.Entity<User>();
+    userEntity.ToTable("Users");
+    userEntity.HasKey(p => p.Id);
+    userEntity.Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+    userEntity.Property(p => p.FullName).IsRequired().HasMaxLength(256);
+    userEntity.Property(p => p.PreferredName).IsRequired().HasMaxLength(256);
+    userEntity.Property(p => p.Email).IsRequired().HasMaxLength(256);
+    userEntity.Property(p => p.Birthdate).IsRequired();
 
     builder.UseSnakeCase();
   }
