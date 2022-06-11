65752667a878db016bd1111f48b86b7bd12bdaa9
Kolya Castañeda <gakol8@hotmail.com>
2022-06-10 21:47:08 -0500

    fix(users): implemented post method

--- WAW.API/Auth/Controllers/UserController.cs
+++ WAW.API/Auth/Controllers/UserController.cs
@@ -33,6 +33,16 @@ public async Task<IActionResult> Post([FromBody] UserRequest resource) {
   var result = await service.Create(user);
   return result.ToResponse<UserResource>(this, mapper);
 }
+[HttpPut("{id:int}")]
+public async Task<IActionResult> Put(int id, [FromBody] UserRequest resource) {
+  if (!ModelState.IsValid) {
+    return BadRequest(ModelState.GetErrorMessages());
+  }
+
+  var user = mapper.Map<UserRequest, User>(resource);
+  var result = await service.Update(id, user);
+  return result.ToResponse<UserResource>(this, mapper);
+}
 [HttpDelete("{id:int}")]
 public async Task<IActionResult> DeleteAsync(int id) {
   var result = await service.Delete(id);
