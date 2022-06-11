10fb4310f6a7768f7a19b0fd330233ee03359cb4
Angel Meneses <angelmenesest123@gmail.com>
2022-06-11 05:31:35 -0500

    docs(jobs): added swagger schemas to Job Offer DTOs

--- WAW.API/Job/Resources/OfferRequest.cs
+++ WAW.API/Job/Resources/OfferRequest.cs
@@ -1,19 +1,26 @@
 using System.ComponentModel.DataAnnotations;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace WAW.API.Job.Resources;
 
+[SwaggerSchema(Required = new[]{"Title", "Description", "SalaryRange", "Status",})]
 public class OfferRequest {
+  [SwaggerSchema("Job offer title",  Nullable = false)]
   [Required]
-  public string? Title { get; set; }
+  public string Title { get; set; } = string.Empty;
 
-  [Required]
+  [SwaggerSchema("Job offer image URL", Nullable = false)]
   public string? Image { get; set; }
 
+  [SwaggerSchema("Job offer description", Nullable = false)]
   [Required]
-  public string? Description { get; set; }
+  public string Description { get; set; } = string.Empty;
 
-  public string? SalaryRange { get; set; }
-
+  [SwaggerSchema("Job offer salary range", Nullable = true)]
   [Required]
-  public bool? Status { get; set; }
+  public string SalaryRange { get; set; } = string.Empty;
+
+  [SwaggerSchema("Job offer status", Nullable = false)]
+  [Required]
+  public bool Status { get; set; }
 }
--- WAW.API/Job/Resources/OfferResource.cs
+++ WAW.API/Job/Resources/OfferResource.cs
@@ -1,10 +1,23 @@
+using Swashbuckle.AspNetCore.Annotations;
+
 namespace WAW.API.Job.Resources;
 
 public class OfferResource {
+  [SwaggerSchema("Job offer identifier", ReadOnly = true)]
   public long Id { get; set; }
+
+  [SwaggerSchema("Job offer title")]
   public string Title { get; set; } = string.Empty;
+
+  [SwaggerSchema("Job offer image URL")]
   public string? Image { get; set; }
-  public string? Description { get; set; }
+
+  [SwaggerSchema("Job offer description")]
+  public string Description { get; set; } = string.Empty;
+
+  [SwaggerSchema("Job offer salary range")]
   public string SalaryRange { get; set; } = string.Empty;
+
+  [SwaggerSchema("Job offer status")]
   public bool Status { get; set; }
 }
