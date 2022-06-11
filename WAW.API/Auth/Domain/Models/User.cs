c6dc66465e84594d8764eb3db4397c3693cdab4b
Kolya Castañeda <gakol8@hotmail.com>
2022-06-10 20:51:02 -0500

    fix(users): modified nullable properties

--- WAW.API/Auth/Domain/Models/User.cs
+++ WAW.API/Auth/Domain/Models/User.cs
@@ -9,9 +9,9 @@ public class User: BaseModel {
   public string PreferredName { get; set; }
   public string Email { get; set; }
   public DateOnly Birthdate { get; set; }
-  public string Location { get; set; }
-  public string Biography { get; set; }
-  public string About { get; set; }
+  public string? Location { get; set; }
+  public string? Biography { get; set; }
+  public string? About { get; set; }
 
 
 
