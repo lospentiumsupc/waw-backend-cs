2102dccc435a443dca6f250d6531ac5088f610d5
Kolya Castañeda <gakol8@hotmail.com>
2022-06-10 20:05:45 -0500

    feat(user): created users model

---
+++ WAW.API/Auth/Domain/Models/User.cs
@@ -0,0 +1,23 @@
+
+using WAW.API.Shared.Domain.Model;
+
+namespace WAW.API.Auth.Domain.Models;
+
+public class User: BaseModel {
+
+  public string FullName { get; set; }
+  public string PreferredName { get; set; }
+  public string Email { get; set; }
+  public DateOnly Birthdate { get; set; }
+  public string Location { get; set; }
+  public string Biography { get; set; }
+  public string About { get; set; }
+  public int ProfileViews { get; set; }
+
+
+
+
+
+
+
+}
