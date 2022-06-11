29494067526e5bb3a68337c4f723332394d96ef3
Angel Meneses <angelmenesest123@gmail.com>
2022-06-11 05:31:58 -0500

    docs(users): added swagger schemas to Users DTOs

--- WAW.API/Auth/Resources/UserRequest.cs
+++ WAW.API/Auth/Resources/UserRequest.cs
@@ -1,23 +1,33 @@
 using System.ComponentModel.DataAnnotations;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace WAW.API.Auth.Resources;
 
+[SwaggerSchema(Required = new[]{"FullName", "PreferredName", "Email", "Birthdate", "Location",})]
 public class UserRequest {
+  [SwaggerSchema("User fullname", Nullable = false)]
   [Required]
   public string FullName { get; set; } = string.Empty;
 
+  [SwaggerSchema("User preferred name to use in the app", Nullable = false)]
   [Required]
   public string PreferredName { get; set; } = string.Empty;
 
+  [SwaggerSchema("User email", Nullable = false)]
   [Required]
   public string Email { get; set; } = string.Empty;
 
+  [SwaggerSchema("User birthdate", Nullable = false)]
   [Required]
   public DateOnly Birthdate { get; set; }
 
+  [SwaggerSchema("User location (address, city or country)", Nullable = false)]
   [Required]
   public string Location { get; set; } = string.Empty;
 
+  [SwaggerSchema("User biography", Nullable = true)]
   public string Biography { get; set; } = string.Empty;
+
+  [SwaggerSchema("User abstract", Nullable = true)]
   public string About { get; set; } = string.Empty;
 }
--- WAW.API/Auth/Resources/UserResource.cs
+++ WAW.API/Auth/Resources/UserResource.cs
@@ -1,12 +1,29 @@
+using Swashbuckle.AspNetCore.Annotations;
+
 namespace WAW.API.Auth.Resources;
 
 public class UserResource {
+  [SwaggerSchema("User identifier", ReadOnly = true)]
   public long Id { get; set; }
+
+  [SwaggerSchema("User fullname")]
   public string FullName { get; set; } = string.Empty;
+
+  [SwaggerSchema("User preferred name to use in the app")]
   public string PreferredName { get; set; } = string.Empty;
+
+  [SwaggerSchema("User email")]
   public string Email { get; set; } = string.Empty;
+
+  [SwaggerSchema("User birthdate")]
   public DateOnly Birthdate { get; set; }
+
+  [SwaggerSchema("User location")]
   public string Location { get; set; } = string.Empty;
+
+  [SwaggerSchema("User biography")]
   public string Biography { get; set; } = string.Empty;
+
+  [SwaggerSchema("User abstract")]
   public string About { get; set; } = string.Empty;
 }
