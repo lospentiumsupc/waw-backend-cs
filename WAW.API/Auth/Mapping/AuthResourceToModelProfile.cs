1ee0f051051ffaeafc19e77a4491e1de631edf1d
FrancoVasquez <u201920339@upc.edu.pe>
2022-06-24 16:33:21 -0500

    fix(auth): fix source for auth mapping

--- WAW.API/Auth/Mapping/AuthResourceToModelProfile.cs
+++ WAW.API/Auth/Mapping/AuthResourceToModelProfile.cs
@@ -1,14 +1,11 @@
 using AutoMapper;
 using WAW.API.Auth.Domain.Models;
 using WAW.API.Auth.Resources;
-using WAW.API.Chat.Domain.Models;
-using WAW.API.Chat.Resources;
 
 namespace WAW.API.Auth.Mapping;
 
 public static class AuthResourceToModelProfile {
   public static void Register(IProfileExpression profile) {
-    profile.CreateMap<ChatRoomRequest, ChatRoom>();
-    profile.CreateMap<MessageRequest, Message>();
+    profile.CreateMap<UserRequest, User>();
   }
 }
