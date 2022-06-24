64f91d674603fb00315c29ef3feb26990177dff7
FrancoVasquez <u201920339@upc.edu.pe>
2022-06-24 16:42:38 -0500

    feat(chat): chat injections services implementation

---
+++ WAW.API/Chat/Injection/ChatInjections.cs
@@ -0,0 +1,16 @@
+using WAW.API.Chat.Domain.Models;
+using WAW.API.Chat.Domain.Repositories;
+using WAW.API.Chat.Persistence.Repositories;
+using WAW.API.Chat.Domain.Services;
+using WAW.API.Chat.Services;
+
+namespace WAW.API.Chat.Injection;
+
+public class ChatInjections {
+  public static void Register(IServiceCollection services) {
+    services.AddScoped<IChatRoomRepository, ChatRoomRepository>();
+    services.AddScoped<IChatRoomService, ChatRoomService>();
+    services.AddScoped<IMessageRepository, MessageRepository>();
+    services.AddScoped<IMessageService, MessageService>();
+  }
+}
