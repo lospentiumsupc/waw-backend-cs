49aec5fe022228e5f38dd8b2b8b797a802cf6679
FrancoVasquez <u201920339@upc.edu.pe>
2022-06-24 16:30:10 -0500

    fix: mapping reimplementation

---
+++ WAW.API/Chat/Mapping/ChatResourceToModelProfile.cs
@@ -0,0 +1,11 @@
+using AutoMapper;
+using WAW.API.Chat.Domain.Models;
+using WAW.API.Chat.Resources;
+
+namespace WAW.API.Chat.Mapping;
+
+public class ChatResourceToModelProfile {
+  public static void Register(IProfileExpression profile) {
+    profile.CreateMap<ChatRoomRequest, ChatRoom>();
+  }
+}
