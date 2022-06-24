643f21cbd68232b2ab9cb3122093dc10520f2957
FrancoVasquez <u201920339@upc.edu.pe>
2022-06-24 17:00:15 -0500

    fix(chat): reimplementation of model of chat room

--- WAW.API/Chat/Domain/Models/ChatRoom.cs
+++ WAW.API/Chat/Domain/Models/ChatRoom.cs
@@ -4,7 +4,8 @@ using WAW.API.Shared.Domain.Model;
 namespace WAW.API.Chat.Domain.Models;
 
 public class ChatRoom : BaseModel {
-  public DateTime Date { get; set; }
+  public DateTime CreationDate { get; set; }
+  public DateTime LastUpdateDate { get; set; }
 
   //Relationship
   public IList<Message> Messages { get; set; } = new List<Message>();
