35f70d9af3da6c256fa4ef244aa6eedc204d932f
FrancoVasquez <u201920339@upc.edu.pe>
2022-06-24 16:30:43 -0500

    feat(chat): mapping implementation on shared section

--- WAW.API/Auth/Mapping/AuthResourceToModelProfile.cs
+++ WAW.API/Auth/Mapping/AuthResourceToModelProfile.cs
@@ -1,11 +1,14 @@
 using AutoMapper;
 using WAW.API.Auth.Domain.Models;
 using WAW.API.Auth.Resources;
+using WAW.API.Chat.Domain.Models;
+using WAW.API.Chat.Resources;
 
 namespace WAW.API.Auth.Mapping;
 
 public static class AuthResourceToModelProfile {
   public static void Register(IProfileExpression profile) {
-    profile.CreateMap<UserRequest, User>();
+    profile.CreateMap<ChatRoomRequest, ChatRoom>();
+    profile.CreateMap<MessageRequest, Message>();
   }
 }
