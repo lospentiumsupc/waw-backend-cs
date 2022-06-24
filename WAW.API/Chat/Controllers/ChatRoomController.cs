9a51a663743c17b46d4241e001d157bdb632fd8f
FrancoVasquez <u201920339@upc.edu.pe>
2022-06-24 00:54:59 -0500

    feat: get method implementation

--- WAW.API/Chat/Controllers/ChatRoomController.cs
+++ WAW.API/Chat/Controllers/ChatRoomController.cs
@@ -1,5 +1,33 @@
-namespace WAW.API.Chat.Controllers; 
+using System.Net.Mime;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using WAW.API.Chat.Domain.Models;
+using WAW.API.Chat.Domain.Services;
+using WAW.API.Chat.Domain.Services.Communication;
+using WAW.API.Chat.Resources;
+using WAW.API.Shared.Extensions;
 
-public class ChatRoomController {
-  
+namespace WAW.API.Chat.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+[SwaggerTag("Create, read, update and delete ChatRooms")]
+public class ChatRoomController : ControllerBase {
+  private readonly IChatRoomService service;
+  private readonly IMapper mapper;
+
+  public ChatRoomController(IChatRoomService service, IMapper mapper) {
+    this.service = service;
+    this.mapper = mapper;
+  }
+
+  [HttpGet]
+  [ProducesResponseType(typeof(IEnumerable<ChatRoomResource>), 200)]
+  [SwaggerResponse(200, "All the stored chatRooms were retrieved successfully.", typeof(IEnumerable<ChatRoomResource>))]
+  public async Task<IEnumerable<ChatRoomResource>> GetAll() {
+    var chatRooms = await service.ListAll();
+    return mapper.Map<IEnumerable<ChatRoom>, IEnumerable<ChatRoomResource>>(chatRooms);
+  }
 }
