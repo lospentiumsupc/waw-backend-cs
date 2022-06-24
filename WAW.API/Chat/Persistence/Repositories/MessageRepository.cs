f102491e0e45932ab5afc1244fcfa8dff25b359d
FrancoVasquez <u201920339@upc.edu.pe>
2022-06-23 22:28:50 -0500

    feat: message repository implementation

--- WAW.API/Chat/Persistence/Repositories/MessageRepository.cs
+++ WAW.API/Chat/Persistence/Repositories/MessageRepository.cs
@@ -1,5 +1,27 @@
-namespace WAW.API.Chat.Persistence.Repositories; 
+using Microsoft.EntityFrameworkCore;
+using WAW.API.Chat.Domain.Models;
+using WAW.API.Chat.Domain.Repositories;
+using WAW.API.Shared.Persistence.Contexts;
+using WAW.API.Shared.Persistence.Repositories;
 
-public class MessageRepository {
-  
+namespace WAW.API.Chat.Persistence.Repositories;
+
+public class MessageRepository : BaseRepository, IMessageRepository {
+  public MessageRepository(AppDbContext context) : base(context) {}
+
+  public async Task<IEnumerable<Message>> ListAll() {
+    return await context.Messages.ToListAsync();
+  }
+
+  public async Task Add(Message message) {
+    await context.Messages.AddAsync(message);
+  }
+
+  public async Task<Message?> FindById(long id) {
+    return await context.Messages.FindAsync(id);
+  }
+
+  public void Remove(Message message) {
+    context.Messages.Remove(message);
+  }
 }
