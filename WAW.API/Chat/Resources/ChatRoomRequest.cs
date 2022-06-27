f5bb2fac8096158a5e332ce28a83ced5c30345e5
FrancoVasquez <u201920339@upc.edu.pe>
2022-06-27 10:55:23 -0500

    fix: remove list of messages from the request and resource

--- WAW.API/Chat/Resources/ChatRoomRequest.cs
+++ WAW.API/Chat/Resources/ChatRoomRequest.cs
@@ -9,7 +9,4 @@ public class ChatRoomRequest {
   [SwaggerSchema("Chat room creation date", Nullable = false)]
   [Required]
   public IList<User> Participants { get; set; } = new List<User>();
-  [SwaggerSchema("Chat room creation date", Nullable = false)]
-  [Required]
-  public IList<Message> Messages { get; set; } = new List<Message>();
 }
--- WAW.API/Chat/Resources/ChatRoomResource.cs
+++ WAW.API/Chat/Resources/ChatRoomResource.cs
@@ -16,7 +16,4 @@ public class ChatRoomResource {
 
   [SwaggerSchema("Chat room last update date", Nullable = false)]
   public IList<User> Participants { get; set; } = new List<User>();
-
-  [SwaggerSchema("Chat room last update date", Nullable = false)]
-  public IList<Message> Messages { get; set; } = new List<Message>();
 }
